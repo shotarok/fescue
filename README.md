1fe0a9cda86ddd076935cde4b092f9f0f7574971
Shotaro Kohama <khmshtr28@gmail.com>
2022-10-31 23:49:01 -0700

    feat: add a script to generate calendar heatmap in the past 30 days

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # fescue
-Calendar Heatmap Generation of the read articles on Feedly
+Calendar Heatmap Generation of the read articles on Feedly.
+
+```bash
+go run main.go -file token.json -date 2022-10-31 | ~/go/bin/calendarheatmap > chart.png
+```
+
+![](./chart.png)
+
+[NOTE] Please take a look at [sfanous/go-feedly](https://github.com/sfanous/go-feedly) for the format of token.json
--- main.go
+++ main.go
@@ -14,6 +14,8 @@ import (
 	"golang.org/x/oauth2"
 )
 
+const DAYS = 30
+
 func fetchToken(filename string) (*oauth2.Token, error) {
 	b, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -38,6 +40,22 @@ func getLatestRead(f *feedly.Client, d time.Time) (res *feedly.MarkerLatestReadR
 	return latestReadRes, nil
 }
 
+func Diff(lhs *hashset.Set[string], rhs *hashset.Set[string]) *hashset.Set[string] {
+	s := hashset.New[string]()
+	for _, v := range lhs.Values() {
+		if !rhs.Contains(v) {
+			s.Add(v)
+		}
+	}
+
+	for _, v := range rhs.Values() {
+		if !lhs.Contains(v) {
+			s.Add(v)
+		}
+	}
+	return s
+}
+
 func main() {
 	var (
 		filename string
@@ -56,47 +74,34 @@ func main() {
 	}
 
 	f := feedly.NewClient(oauth2.NewClient(context.Background(), oauth2.StaticTokenSource(oauth2Token)))
-
 	t, err := time.Parse("2006-01-02", date)
 	if err != nil {
 		fmt.Printf("Failed to parse a given arg: %v", err)
 	}
-	tPlusOne := t.Add(time.Duration(1) * time.Hour * 24)
 
-	resAtT, err := getLatestRead(f, t)
-	if err != nil {
-		fmt.Printf("Failed to get the latest read articles: %v", err)
-		return
-	}
-	resAtTPlusOne, err := getLatestRead(f, tPlusOne)
-	if err != nil {
-		fmt.Printf("Failed to get the latest read articles: %v", err)
+	m := make(map[string]int)
+	var prev *hashset.Set[string]
+	for i := 0; i < DAYS; i++ {
+		d := t.Add(time.Duration(-i) * time.Hour * 24)
+		res, err := getLatestRead(f, d)
+		if err != nil {
+			fmt.Printf("Failed to get the latest read articles: %v", err)
+			return
+		}
+		cur := hashset.New[string](res.Entries...)
+		if prev != nil {
+			diff := Diff(cur, prev)
+			m[d.Format("2006-01-02")] = diff.Size()
+		}
+		prev = cur
 	}
 
-	p := func(r *feedly.MarkerLatestReadResponse) {
+	p := func(r interface{}) {
 		b, err := json.MarshalIndent(r, "", "    ")
 		if err != nil {
 			fmt.Printf("Failed to marshal latestReadRes: %v", err)
 		}
 		fmt.Println(string(b))
 	}
-	p(resAtT)
-	p(resAtTPlusOne)
-
-	settPlusOne := hashset.New[string]()
-	for _, v := range resAtTPlusOne.Entries {
-		settPlusOne.Add(v)
-	}
-
-	diff := hashset.New[string]()
-	for _, v := range resAtT.Entries {
-		if !settPlusOne.Contains(v) {
-			diff.Add(v)
-		}
-	}
-
-	fmt.Printf("Read Article Count on %s: %d\n", t.Format("2006-01-02"), diff.Size())
-	for i, v := range diff.Values() {
-		fmt.Printf("entry %d: %s\n", i, v)
-	}
+	p(m)
 }
